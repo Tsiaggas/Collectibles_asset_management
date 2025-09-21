ab9721ddf1900139a4fcc7751a61c93cf888a181
Sakis_Tsiaggas <ttsiaggas94@gmail.com>
2025-09-21 13:07:54 +0300

    02

--- src/App.tsx
+++ src/App.tsx
@@ -513,11 +513,24 @@ Single\tGengar\tFossil\tLP\t39.9\tyes\ttrue\t0\tInactive\t\tshadow`}
                 .map((img, index) => (
                   <div key={index} className="relative group aspect-square">
                     <img src={img.url} alt={img.label} className="w-full h-full object-cover rounded-lg border dark:border-gray-600" />
-                    <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center rounded-lg">
+                    <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center gap-2 rounded-lg">
+                      {/* Download Button */}
+                      <a 
+                        href={img.url!}
+                        download
+                        onClick={(e) => e.stopPropagation()}
+                        className="text-white p-2 bg-blue-600 rounded-full hover:bg-blue-700 transition-colors"
+                        aria-label="Download image"
+                        title="Download image"
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                      </a>
+                      {/* Delete Button */}
                       <button 
-                        onClick={() => handleDeleteImage(edit.item!, img.url!)}
+                        onClick={(e) => { e.stopPropagation(); handleDeleteImage(edit.item!, img.url!); }}
                         className="text-white p-2 bg-red-600 rounded-full hover:bg-red-700 transition-colors"
                         aria-label="Delete image"
+                        title="Delete image"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" /></svg>
                       </button>
